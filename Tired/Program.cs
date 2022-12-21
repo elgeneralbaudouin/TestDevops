bc400710003b54d885adb1104c5660dbaa629344
Dongmo Baudouin <32611531+elgeneralbaudouin@users.noreply.github.com>
2022-12-21 20:01:56 +0100

    Update Program.cs

--- Tired/Program.cs
+++ Tired/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("je suis fatigué de tester ngrok good");
+Console.WriteLine("je suis fatigué de tester ngrok goods");
